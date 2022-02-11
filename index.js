f6e4ce05e23e802aba7a0d8e9f3bb5bb3cd96348
Igor Pedro <igorvspedro73@gmail.com>
2022-02-11 16:09:35 -0300

    calculando IMC e exibindo alerta do resultado

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 (function () {
+    const altura = document.querySelector('#altura');
+    const peso = document.querySelector('#peso');
+    const botao = document.querySelector('button')
+
+    botao.onclick = function () {
+        const resultado = calcularIMC(parseFloat(altura.value) , parseFloat(peso.value))
+        alert('O IMC é ' + resultado)
+    }
 
 })();
+
+function calcularIMC(altura, peso) {
+    const alturaAoQuadrado = altura * altura 
+    return (peso / alturaAoQuadrado).toFixed(2)
+} 
+
