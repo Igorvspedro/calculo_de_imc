02bfabcc3dd1334cbd841f64a44e2f2bc820f333
Igor Pedro <igorvspedro73@gmail.com>
2022-02-11 16:18:01 -0300

    Corrigindo a falta de ponto e virgula nos finais das linhas

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 (function () {
     const altura = document.querySelector('#altura');
     const peso = document.querySelector('#peso');
-    const botao = document.querySelector('button')
+    const botao = document.querySelector('button');
 
     botao.onclick = function () {
-        const resultado = calcularIMC(parseFloat(altura.value) , parseFloat(peso.value))
-        alert('O IMC é ' + resultado)
+        const resultado = calcularIMC(parseFloat(altura.value) , parseFloat(peso.value));
+        alert('O IMC é ' + resultado);
     }
 
 })();
 
 function calcularIMC(altura, peso) {
-    const alturaAoQuadrado = altura * altura 
-    return (peso / alturaAoQuadrado).toFixed(2)
+    const alturaAoQuadrado = altura * altura ;
+    return (peso / alturaAoQuadrado).toFixed(2);
 } 
 
